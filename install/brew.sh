f3e3e2a9217b26788ebb718fa69a16ee9980f618
Ashley Roach <aroach@gmail.com>
2015-06-22 08:15:47 -0600

    add more install tasks

---
+++ install/brew.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# Update Homebrew, formulae, and packages
+
+brew update
+brew upgrade
+
+# Install packages
+
+apps=(
+    bash-completion2
+    coreutils
+    dockutil
+    ffmpeg
+    fsad
+    gifsicle
+    git
+    gnu-sed --default-names
+    grep --default-names
+    hub
+    imagemagick
+    jq
+    mackup
+    node
+    peco
+    phantomjs
+    pngquant
+    psgrep
+    python
+    shellcheck
+    ssh-copy-id
+    svn
+    tree
+    vim
+    wget
+)
+
+brew install "${apps[@]}"
---
+++ install/osx.sh
@@ -0,0 +1,45 @@
+#install Homebrew & brew-cask
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew tap caskroom/cask
+brew install brew-cask
+brew tap homebrew/versions
+brew tap caskroom/versions
+
+# Install brew & brew-cask packages
+
+. "$DOTFILES_DIR/install/brew.sh"
+. "$DOTFILES_DIR/install/brew-cask.sh"
+
+# Install bash (with Homebrew)
+
+#. "$DOTFILES_DIR/install/bash.sh"
+
+# Install Ruby gems (SASS, Compass)
+
+#sudo gem install compass
+#sudo gem install lunchy
+#sudo gem install pygmentize
+#sudo gem install sass
+#sudo gem install sass-globbing
+
+# Globally install with npm
+
+npm install -g bower
+npm install -g grunt
+npm install -g gulp
+npm install -g http-server
+npm install -g nodemon
+npm install -g tldr
+npm install -g spot
+#npm install -g svgo
+npm install -g vtop
+
+# http://www.sublimetext.com/docs/3/osx_command_line.html
+#[ -f ~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl ] && ln -sfv ~/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl "$DOTFILES_DIR/bin/subl"
+
+# Install extra stuff
+
+if [ -d "$EXTRA_DIR" -a -f "$EXTRA_DIR/install/osx.sh" ]; then
+    . "$EXTRA_DIR/install/osx.sh"
+fi
